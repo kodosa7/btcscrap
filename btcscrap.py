4bce6faccf76312ed669c371b0a74af5f4d639c1
andrea6128 <Spectrum128!!>
2021-01-26 17:11:05 +0100

    requirements.txt added

---
+++ btcscrap.py
@@ -0,0 +1,30 @@
+# btc rise scrapper
+# (c) 01/2021 Aja
+# scraps given websites for given string matches
+
+import requests, re
+from bs4 import BeautifulSoup as bs
+from data import urls, btc_strings, strings
+
+
+# Bitcoin rise scrapping app
+print("Bitcoin Rise Web Scrapper (c) 2021 Aja")
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"
+}
+
+for url in urls:
+    current_site = requests.get(url, headers=headers)
+
+    soup = bs(current_site.content, 'html.parser')
+
+    for btc_string in btc_strings:
+        btc_nibble = soup.find(text=re.compile(btc_string))
+
+        if btc_nibble == None:
+            btc_nibble = ""
+
+        for string in strings:
+            if string in btc_nibble and not "{" in btc_nibble:
+                print("\nFound: ", btc_nibble)
+                print("Site: ", url)
---
+++ data.py
@@ -0,0 +1,17 @@
+# data file for bitcoin rise web scrapper
+
+urls = ["https://idnes.cz/", "https://novinky.cz/", "https://www.e15.cz/",
+        "https://cryptosvet.cz/"]
+
+btc_strings = ["Bitcoin", "bitcoin", "Bitcoinu", "bitcoinu",
+               "Bitcoinem", "bitcoinem",
+               "BTC", "btc", "XBT", "xbt"]
+
+strings = ["Price", "price", "Value", "value",
+            "Grow", "grow", "Soar", "soar", "Rise", "rise",
+            "Grows", "grows", "Soars", "soars", "Rises", "rises",
+            "cena", "stoupá", "vzestup", "vzestupu", "stoupající", "stoupat",
+            "neklesá", "neklesající", "vrátil", "vrací", "vrátí", "vrátit",
+            "stoupnul", "stoupne", "dosáhnul", "dosáhne", "dosáhnout",
+            "růst", "roste", "vzrůst", "vzroste", "rostoucí", "vzrůstající",
+            "pokles"]
